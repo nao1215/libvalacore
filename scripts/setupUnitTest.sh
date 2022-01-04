9477195f61330960121d0ca1c9d777c1bdb34d33
Nao1215 <n.chika156@gmail.com>
2022-01-04 21:31:51 +0900

    Add unit test for canRead() / canWrite() / canExec() in File class.

--- scripts/setupUnitTest.sh
+++ scripts/setupUnitTest.sh
@@ -6,10 +6,24 @@ UT_DIR="/tmp/valacore/ut"
 
 function mkUnitTestDir() {
     mkdir -p ${UT_DIR}
+    mkdir -p ${UT_DIR}/canNotReadDir
+    mkdir -p ${UT_DIR}/canNotWriteDir
+    mkdir -p ${UT_DIR}/canNotExecDir
+    chmod 777 ${UT_DIR}
+    chmod a-r ${UT_DIR}/canNotReadDir
+    chmod a-w ${UT_DIR}/canNotWriteDir
+    chmod a-x ${UT_DIR}/canNotExecDir
 }
 
 function mkTestFile() {
     touch ${UT_DIR}/file.txt
+    touch ${UT_DIR}/canNotRead.txt
+    touch ${UT_DIR}/canNotWrite.txt
+    touch ${UT_DIR}/canNotExec.txt
+    chmod 777 ${UT_DIR}/file.txt
+    chmod a-r ${UT_DIR}/canNotRead.txt
+    chmod a-w ${UT_DIR}/canNotWrite.txt
+    chmod a-x ${UT_DIR}/canNotExec.txt
 }
 
 mkUnitTestDir
